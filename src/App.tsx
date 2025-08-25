c19a9ce98e118deea80ce550f32fe72b6ae2de21
Taras <luchakt2@gmail.com>
2025-08-25 22:31:18 +0300

    fixed handleKeyAp to handleKeyUp

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,12 @@ type State = {
 };
 
 export class App extends React.Component<{}, State> {
-
   state: State = {
     pressedKey: null,
-  }
+  };
 
   // Обробник події, який буде викликатися при натисканні клавіші
-  handleKeyAp = (event: KeyboardEvent) => {
+  handleKeyUp = (event: KeyboardEvent) => {
     // 1. Отримуємо ключ натиснутої клавіші
     const pressedKey = event.key;
 
@@ -21,13 +20,13 @@ export class App extends React.Component<{}, State> {
 
   componentDidMount() {
     // 3. Додаємо слухач події до документа
-    document.addEventListener('keyup', this.handleKeyAp);
+    document.addEventListener('keyup', this.handleKeyUp);
   }
 
   // 4. Додаємо метод життєвого циклу componentWillUnmount
   componentWillUnmount() {
     // 2. Видаляємо слухач події
-    document.removeEventListener('keyup', this.handleKeyAp);
+    document.removeEventListener('keyup', this.handleKeyUp);
   }
 
   // 1. Оновлюємо метод render()
